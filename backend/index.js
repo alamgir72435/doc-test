18c265e4c4da144781ef12aeb82a196acb0339e1
Alamgir Ahamed <alamgir72435@gmail.com>
2022-10-21 20:25:38 +0600

    Updates

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,14 @@ const app = express();
 // Redis Connection
 
 
-const redisClient = redis.createClient(6379,'redis');
+const redisClient = redis.createClient({
+  socket:{
+    host:process.env.REDIS_HOST,
+    port:process.env.REDIS_PORT,
+    
+  },
+  password:''
+});
 
 redisClient.connect().then(() => {
   console.log('Redis Connected');
